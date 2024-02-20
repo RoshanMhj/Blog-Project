f75620a929872acfb75d85a76637abed09570872
Zaleeeeeeee <mhroshan778@gmail.com>
2024-02-20 20:38:30 +0545

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,25 @@
 import "./App.css";
+import { Home } from "./Pages/Home/Home";
+import { About } from "./Pages/About/About";
+import { Dashboard } from "./Pages/Dashboard/Dashboard";
+import { Projects } from "./Pages/Projects/Projects";
+import { SignIn } from "./Pages/SignIn/SignIn";
+import { SignUp } from "./Pages/SignUp/SignUp";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1>Blog</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/Pages/SignIn/SignIn.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./SignIn.css";
+
+export const SignIn = () => {
+  return <div>SignIn</div>;
+};
