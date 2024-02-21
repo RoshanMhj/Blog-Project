ed2d639d72510f7feec466ddbaf7a27219c21c97
Zaleeeeeeee <mhroshan778@gmail.com>
2024-02-21 18:40:02 +0545

    connected database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(
+    "mongodb+srv://roshanzale:roshanzale@blog-project.krruzrd.mongodb.net/blog-project?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
