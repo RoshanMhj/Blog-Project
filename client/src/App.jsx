a04a231e5beeb3981ebfcc86d2d7e90d161cafe6
Zaleeeeeeee <mhroshan778@gmail.com>
2024-02-21 17:58:34 +0545

    completed navigation bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,14 @@ import { Dashboard } from "./Pages/Dashboard/Dashboard";
 import { Projects } from "./Pages/Projects/Projects";
 import { SignIn } from "./Pages/SignIn/SignIn";
 import { SignUp } from "./Pages/SignUp/SignUp";
+import { Header } from "./Components/Header/Header";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ client/src/Components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Header.css";
+import { NavLink, Link } from "react-router-dom";
+import { CiSearch } from "react-icons/ci";
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <nav className="nav">
+        <Link to="/" className="link link-brand">
+          <span className="main-tag">VERVE</span>
+        </Link>
+        <div className="search">
+          <input className="header-input" type="text" placeholder="Search..." />
+          <CiSearch className="search" />
+        </div>
+        <div className="links">
+          <NavLink to="/" className="link-second">
+            <span className="link-items">Home</span>
+          </NavLink>
+
+          <NavLink to="/about" className="link-second">
+            <span className="link-items">About</span>
+          </NavLink>
+          <NavLink to="/projects" className="link-second">
+            <span className="link-items">Projects</span>
+          </NavLink>
+        </div>
+
+        <button className="sign-in">
+          <Link className="link" to="/sign-in">
+            Sign In
+          </Link>
+        </button>
+      </nav>
+    </div>
+  );
+};
