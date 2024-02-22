98b78371888be248a277b7fe24c0fff41e7b2001
Zaleeeeeeee <mhroshan778@gmail.com>
2024-02-22 14:40:06 +0545

    created a skeleton for login page

--- client/src/Pages/SignUp/SignUp.jsx
+++ client/src/Pages/SignUp/SignUp.jsx
@@ -1,6 +1,34 @@
 import React from "react";
 import "./SignUp.css";
+import { Link } from "react-router-dom";
 
 export const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="signup">
+      <div className="singup-brand">
+        <div className="brand">
+          <p>VERVE</p>
+        </div>
+        <p>
+          Join our diverse blog community, where voices thrive. Whether a
+          seasoned writer or novice, share ideas, connect, and embark on
+          inspiration together.
+        </p>
+      </div>
+      <div className="signup-info">
+        <form>
+          <p>Your Username: </p>
+          <input type="text" placeholder="Username" id="username" />
+          <p>Your Email:</p>
+          <input type="email" placeholder="name@company.com" id="email" />
+          <p>Your Password</p>
+          <input type="password" placeholder="Your Password" id="password" />
+
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+      <span>Already have an account?</span>
+      <Link to="/sign-up">SignUp</Link>
+    </div>
+  );
 };
